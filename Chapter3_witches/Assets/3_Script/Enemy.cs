76659ebe5d0a27beb6abe1ce522c02c5e22ff776
junhyungPARK78 <createzone@gmail.com>
2015-09-27 17:09:52 +0900

    2015 09 27 17 10

---
+++ Chapter3_witches/Assets/3_Script/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	public float _speed;
+	public GameObject[] _EnemySetObj;
+
+	// Use this for initialization
+	void Start () {
+	
+
+		_EnemySetObj [0].transform.localPosition += new Vector3 (0, Random.Range (-2, 3) * 130.0f, 0);
+		_EnemySetObj [1].transform.localPosition += new Vector3 (0, Random.Range (-2, 3) * 130.0f, 0);
+		_EnemySetObj [2].transform.localPosition += new Vector3 (0, Random.Range (-2, 3) * 130.0f, 0);
+		_EnemySetObj [3].transform.localPosition += new Vector3 (0, Random.Range (-2, 3) * 130.0f, 0);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		transform.Translate (_speed * Time.deltaTime, 0, 0);
+
+	}
+}
---
+++ Chapter3_witches/Assets/3_Script/GM.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GM : MonoBehaviour {
+
+	public GameObject _enemySet;
+	public GameObject _nearBgObj;
+	public Transform _PlayerObjPool;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+
+	public bool _SpawnChk = true;
+
+	void Update () {
+	
+		if (_nearBgObj.transform.localPosition.x < -2460.0f && _SpawnChk) 
+		{
+
+			var Set1 = Instantiate
+				(_enemySet, Vector3.zero,Quaternion.identity) as GameObject;
+			Set1.transform.parent = _PlayerObjPool;
+			Set1.transform.localScale = new Vector3 (1, 1, 1);
+			Set1.transform.localPosition = Vector3.zero;
+			_SpawnChk =false;
+		
+		}
+
+		if(_nearBgObj.transform.localPosition.x > -1300.0f && !_SpawnChk) 
+		{
+			_SpawnChk = true;
+		}
+
+
+	}
+}
